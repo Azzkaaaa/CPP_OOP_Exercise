4fc7e2b211f8ca367770c98dfb7de8ef7ae2a09d
Azzkaaaa <19623121@std.stei.itb.ac.id>
2025-01-27 15:50:01 +0700

    feat: main

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,26 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
-int main(int argc, char const *argv[])
-{
-    cout << "testingg" << endl;
-    cout << "Coba lagi" << endl;
+class Mahasiswa{
+    public:
+        string nama;
+        string NIM;
+        string jurusan;
+        int IPK;
+};
+
+int main(int argc, char const *argv[]){
+    Mahasiswa data1;
+    data1.nama = "Azka";
+    data1.NIM = "13523137";
+    data1.jurusan = "IF";
+    data1.IPK = 3;
+    cout << "Nama saya adalah: " << data1.nama << endl;
+    cout << "NIM: " << data1.NIM << endl;
+    cout << "Jurusan: " << data1.jurusan << endl;
+    cout << "IPK: " << data1.IPK << endl;
     return 0;
 }
 

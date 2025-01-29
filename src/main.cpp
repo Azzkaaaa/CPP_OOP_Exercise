34f54a5b5df6741bc925efd920430e88552d154f
Azzkaaaa <19623121@std.stei.itb.ac.id>
2025-01-29 16:47:12 +0700

    feat: Destructor

--- src/main.cpp
+++ src/main.cpp
@@ -1,79 +1,70 @@
 #include <iostream>
 #include <string>
-#include <fstream>
 
 using namespace std;
 
-// Method adalah sebuah fungsi
-class Mahasiswa{
+class Player{
     public:
-        string nama;
-        string NIM;
-        string jurusan;
+        string name;
 
-        Mahasiswa(string inputNama, string inputNIM, string inputJurusan){// Constructor (Termasuk Methods)
-            Mahasiswa::nama = inputNama;
-            Mahasiswa::NIM = inputNIM;
-            Mahasiswa::jurusan = inputJurusan;
+        // Constructor
+        Player(const char* name){
+            Player::name = name;
+            cout << "Player " << Player::name << " dibuat" << endl;
         }
 
-        string turnString(){
-            return nama + " " + NIM + " " + jurusan + "\n";
+        // Destructor
+        ~Player(){
+            cout << "Player " << Player::name << " dihapus" << endl;
         }
 };
 
-class DataBase{
-    public:
-        ifstream in;
-        ofstream out;
-        string fileName;
-
-        DataBase(const char* fileName){
-            DataBase::fileName = fileName;
-        }
-
-        void save(Mahasiswa data){
-            DataBase::out.open(DataBase::fileName, ios::app);
-            
-            DataBase::out << data.turnString();
-
-            DataBase::out.close();
-        }
-
-        void tampilkanDataBase(){
-            DataBase::in.open(DataBase::fileName, ios::in);
-            string nama, NIM, jurusan;
-            int i;
-            i = 1;
-            while (!DataBase::in.eof()){
-                DataBase::in >> nama;
-                DataBase::in >> NIM;
-                DataBase::in >> jurusan;
-                cout << i++ << "." << endl;
-                cout << nama << endl;
-                cout << NIM << endl;
-                cout << jurusan << endl;
-            }
-            DataBase::in.close();
-        }
-};
-
-
-int main(int argc, char const *argv[]){
-    string nama, NIM, jurusan;
-    cout << "Masukkan Data Mahasiswa" << endl;
-    cout << "Nama: ";
-    cin >> nama;
-    cout << "NIM: ";
-    cin >> NIM;
-    cout << "Jurusan: ";
-    cin >> jurusan;
-
-
-    Mahasiswa dataMahasiswa = Mahasiswa(nama, NIM, jurusan);
-    DataBase dataBase = DataBase("Data.txt");
-
-    dataBase.tampilkanDataBase();
-    return 0;
+void stackWithPointer(Player* &playerPointer){
+    Player stackPlayer = Player("Azka");
+    playerPointer = &stackPlayer;
 }
 
+void heapWithPointer(Player* &playerPointer){
+    Player* heapPlayer = new Player("Azka");
+    playerPointer = heapPlayer;
+}
+
+Player createStackPlayer(){
+    Player stackPlayer = Player("Azka stack");
+    return stackPlayer;
+}
+
+Player* createHeapPlayer(){
+    Player* heapPlayer = new Player("Azka heap");
+    return heapPlayer;
+}
+
+int main(int argc, char const *argv[]){
+
+    {// Delete stack
+        Player stackPlayer = Player("Azka");
+    }
+
+    {// Deleate Heap
+        Player* heapPlayer = new Player("Azka");
+        delete heapPlayer;
+    }
+
+    cout << "\nMemory Leak" << endl;
+    Player* playerPointer1;
+    stackWithPointer(playerPointer1);
+
+
+    Player* playerPointer2;
+    heapWithPointer(playerPointer2);
+
+    cout << "\nReturn Object: " << endl;
+    Player returnStack = createStackPlayer();
+    cout << returnStack.name << endl;
+
+    cout << "\nReturn Object" << endl;
+    Player* returnHeap = createHeapPlayer();
+    cout << returnHeap->name << endl;
+    delete returnHeap;
+    return 0;
+}
